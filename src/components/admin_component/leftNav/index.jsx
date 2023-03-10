ded30c7cf6c096a9223019251e9c13e2b75290d0
DabBaal <Arideque@outlook.com>
2023-03-11 00:12:57 +0800

    2023年3月11日 Admin组件左侧边栏开发完成

--- src/components/admin_component/leftNav/index.jsx
+++ src/components/admin_component/leftNav/index.jsx
@@ -1,50 +1,76 @@
-import React from 'react'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-import "./left_nav.css"
-import logo from "../../../assets/images/logo.png"
+import "./left_nav.css";
+import logo from "../../../assets/images/logo.png";
+import menuList from "../../../config/menuConfig";
 
-//AntD引入
-import { MailOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-
-//左侧下拉菜单相关函数
-function getItem(label, key, icon, children, type) {
-  return { key, icon, children, label, type, };
-}
-const items = [
-  getItem('首页', '1', <MailOutlined />),
-  getItem('商品', 'sub1', <MailOutlined />, [
-    getItem('品类管理', '2', <MailOutlined />),
-    getItem('商品管理', '3', <MailOutlined />),
-  ]),
-  getItem('用户管理', '4', <MailOutlined />),
-  getItem('角色管理', '5', <MailOutlined />),
-  getItem('图形图表', 'sub2', <MailOutlined />, [
-    getItem('柱形图', '6', <MailOutlined />),
-    getItem('折线图', '7', <MailOutlined />),
-    getItem('饼图', '8', <MailOutlined />),
-  ]),
-];
+import { Menu } from "antd";
 
 export default function LeftNav() {
+  //寻找被选中元素的key属性
+  const path = useLocation().pathname;
+  let defaultSelectedKeys = "", defaultOpenKeys = ""
+
+  menuList.forEach((item) => {
+    if (item.type === "item" && item.href === path) {
+      defaultSelectedKeys = item.key
+    }
+    else if (item.type === "submenu") {
+      item.children.forEach((subItem) => {
+        if (subItem.href === path) {
+          defaultSelectedKeys = subItem.key
+          defaultOpenKeys = item.key
+        }
+      })
+    }
+  })
+
   return (
-    <div className='left_nav'>
+    <div className="left_nav">
       <div className="left_nav_top">
         <img src={logo} alt="logo" />
         <h1>硅谷后台</h1>
       </div>
 
       <div className="left_nav_bottom">
-        <div style={{ width: 200, }}>
+        <div style={{ width: 200 }}>
           <Menu
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={[]}
+            defaultSelectedKeys={[defaultSelectedKeys]}
+            defaultOpenKeys={[defaultOpenKeys]}
             mode="inline"
             theme="dark"
-            items={items}
-          />
+          >
+            {menuList.map((item) => {
+              if (item.type === "item") {
+                return (
+                  <Menu.Item key={item.key} icon={item.icon}>
+                    <Link to={item.href}>{item.label}</Link>
+                  </Menu.Item>
+                );
+              }
+              if (item.type === "submenu") {
+                return (
+                  <Menu.SubMenu
+                    key={item.key}
+                    icon={item.icon}
+                    title={item.label}
+                  >
+                    {item.children.map((subItem) => {
+                      return (
+                        <Menu.Item key={subItem.key} icon={subItem.icon}>
+                          <Link to={subItem.href}>{subItem.label}</Link>
+                        </Menu.Item>
+                      )
+                    })}
+                  </Menu.SubMenu>
+                );
+              }
+              return null;
+            })}
+          </Menu>
         </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ src/config/menuConfig.jsx
@@ -0,0 +1,22 @@
+import { MailOutlined } from "@ant-design/icons";
+
+function getItem(label, key, icon, children, type, href) {
+    return { key, icon, children, label, type, href };
+  }
+  
+  const menuList = [
+    getItem("首页", "1", <MailOutlined />, null, "item", "/"),
+    getItem("商品","sub1",<MailOutlined />,[
+        getItem("品类管理", "2", <MailOutlined />, null, "item", "/category"),
+        getItem("商品管理", "3", <MailOutlined />, null, "item", "/product"),
+      ],"submenu"),
+    getItem("用户管理", "4", <MailOutlined />, null, "item", "/user"),
+    getItem("角色管理", "5", <MailOutlined />, null, "item", "/role"),
+    getItem("图形图表","sub2",<MailOutlined />,[
+        getItem("柱形图", "6", <MailOutlined />, null, "item", "/charts/bar"),
+        getItem("折线图", "7", <MailOutlined />, null, "item", "/charts/line"),
+        getItem("饼图", "8", <MailOutlined />, null, "item", "/charts/pie"),
+      ],"submenu"),
+  ];
+
+  export default menuList
